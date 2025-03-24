dff202abd667866b0ed0780a0d8e6e5eb8150ada
GuyroachThreepbug <PrettyBoy42@proton.me>
2025-03-24 13:40:35 -0400

    changes

--- asteroid.py
+++ asteroid.py
@@ -1,19 +1,42 @@
 from circleshape import CircleShape
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS
+import random
+
 
 class Asteroid(CircleShape):
 
-	def __init__(self, x, y, radius):
-		super().__init__(x, y, radius)
-		self.velocity = pygame.Vector2(50, 50)
-		self.position = pygame.Vector2(x, y)
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(50, 50)
+        self.position = pygame.Vector2(x, y)
 
-	def draw(self, screen):
-		center_arg = int(self.position.x), int(self.position.y)
-		radius = int(self.radius)
-		
-		pygame.draw.circle(screen, (255, 255, 255), center_arg, radius, 2)
-	
-	def update(self,dt):
-		self.position += self.velocity * dt
+    def draw(self, screen):
+        center_arg = int(self.position.x), int(self.position.y)
+        radius = int(self.radius)
+
+        pygame.draw.circle(screen, (255, 255, 255), center_arg, radius, 2)
+
+    def update(self,dt):
+        self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        else:
+            random_angle = random.uniform(20, 50)
+            new_vector1 = self.velocity.rotate(random_angle)
+            new_vector2 = self.velocity.rotate(-random_angle)
+            new_Asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            radius = new_Asteroid_radius
+
+            new_Asteroid1 = Asteroid(self.position.x, self.position.y, radius)
+            new_Asteroid1.velocity = new_vector1 * 1.2
+
+            new_Asteroid2 = Asteroid(self.position.x, self.position.y, radius)
+            new_Asteroid2.velocity = new_vector2 * 1.2
+
+            return new_Asteroid1, new_Asteroid2
--- main.py
+++ main.py
@@ -8,20 +8,6 @@ from circleshape import CircleShape
 from shot import Shot
 
 
-
-def paused(screen):
-
-    font = pygame.font.Font(None, 50)
-    pause_text = font.render("Game Paused", True, (255, 255, 255))
-    resume_text = font.render("Press P to Resume", True, (34, 139, 34))
-    quit_text = font.render("Press Q to Quit", True, (175, 49, 42))
-    screen.blit(pause_text, (SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3))
-    screen.blit(resume_text, (SCREEN_WIDTH / 3, SCREEN_HEIGHT / 2))
-    screen.blit(quit_text, (SCREEN_WIDTH / 3, SCREEN_HEIGHT / 1.5))
-    pygame.display.flip()
-
-
-
 def main():
     pygame.init()
     pygame.font.init()
@@ -60,16 +46,6 @@ def main():
             if event.type == pygame.quit:
                 sys.exit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_p:  # Press 'P' to pause/unpause
-                    is_paused = not is_paused
-            if event.type == pygame.K_q and is_paused:
-                    pygame.quit()
-
-
-        if is_paused:
-            paused(screen)
-
         else:
 
             updatable.update(dt)
@@ -82,7 +58,7 @@ def main():
             for asteroid in asteroid_group:
                 for bullet in clip:
                     if asteroid.collision_check(bullet):
-                        asteroid.kill()
+                        asteroid.split()
                         bullet.kill()
 
 
